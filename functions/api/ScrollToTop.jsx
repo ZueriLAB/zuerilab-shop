f501db233e6278d528f6677da646fce63544910a
ZueriLAB <DEINE-GITHUB-MAIL>
2026-04-13 00:05:51 +0200

    improve scroll to top for mobile

--- functions/api/ScrollToTop.jsx
+++ functions/api/ScrollToTop.jsx
@@ -14,3 +14,17 @@ export default function ScrollToTop() {
 
   return null;
 }
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+export default function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+    document.documentElement.scrollTo(0, 0);
+    document.body.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
